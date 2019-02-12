f9233fb2ef0d63a9dfd56cc29651d52c8f578b60
matkateresa <attwistingnether@gmail.com>
2019-02-12 15:21:45 +0100

    fn

--- src/components/Panel/Entry.jsx
+++ src/components/Panel/Entry.jsx
@@ -2,10 +2,32 @@ import React from "react";
 import "./Entry.css";
 
 export default props => {
+  const [name, setName] = React.useState();
+  const [address, setAddress] = React.useState();
+
+  const nameChangeHandler = e => {
+    setName(e.target.value);
+  };
+
+  const addressChangeHandler = e => {
+    setAddress(e.target.value);
+  };
+
+  const connectToServerHandler = () => {
+    alert(name + "\n" + address);
+  };
+
   return (
     <div className="login">
-      <input type="text" />
-      <input type="text" />
+      <input onChange={nameChangeHandler} className="nameInput" type="text" />
+      <input
+        onChange={addressChangeHandler}
+        className="addressInput"
+        type="text"
+      />
+      <button onClick={connectToServerHandler} className="connectButton">
+        Connect
+      </button>
     </div>
   );
 };
