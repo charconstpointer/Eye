c504f268a10d9184351848df92ed7452e8a6682e
matkateresa <attwistingnether@gmail.com>
2019-02-19 15:59:48 +0100

    j

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,36 @@
 import React, { useState, useRef } from "react";
 import Validator from "../../utils/validation";
+import axios from "axios";
+
 import "./Login.css";
+
 export default props => {
   const userName = useRef();
   const password = useRef();
   const handleLogin = () => {
     if (
       Validator.validateUsername(userName.current.value) &&
-      Validator.validatePassword(password.current.value)
+      // Validator.validatePassword(password.current.value)
+      true
     ) {
-      console.log("gud");
+      let data = JSON.stringify({
+        name: "fuark",
+        password: "mushroom"
+      });
+      axios
+        .post("https://localhost:44326/api/accounts/auth", data, {
+          headers: { "Content-Type": "application/json; charset=utf-8" }
+        })
+        .then(res => {
+          const token = res.data.token;
+          axios
+            .get("https://localhost:44326/api/values/", {
+              headers: { Authorization: `Bearer ${token}` }
+            })
+            .then(res => console.log(res));
+        })
+        .catch(err => console.error(err));
+      console.log(data);
     } else {
       console.log("not");
     }
